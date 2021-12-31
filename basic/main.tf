729007f36c0af288672b5f74d31ec4e272a30569
yuki <yuki.onishi1129@gmail.com>
2022-01-01 03:37:46 +0900

    add: output

--- basic/main.tf
+++ basic/main.tf
@@ -3,16 +3,22 @@
 #     default = "t3.micro"
 # }
 
-locals {
-    example_instance_type = "t3.micro"
-}
+# local変数
+# locals {
+#     example_instance_type = "t3.micro"
+# }
 
 
 
 resource "aws_instance" "example" {
     ami = "ami-0c3fd0f5d33134a76"
+    instance_type = "t3.micro"
     # 変数の使い方
     # instance_type = var.example_instance_type
     # local変数の使い方
-    instance_type = local.example_instance_type
+    # instance_type = local.example_instance_type
+}
+
+output "example_instance_type" {
+    value = aws_instance.example.id
 }
